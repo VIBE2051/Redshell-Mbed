c0ca8203a70759d754965d56eedfc491a1e46e5e
Francisco Jorquera <ejorquera2051@gmail.com>
2024-06-29 14:18:06 +1000

    Speed displayed and set to rpm

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@
 #include "ADXL345.h"
 #include "mbed.h"
 #include <cmath>
+#include <cstdint>
 #include <cstdio>
+
 /////Pin allocations made specifically for NUCLEO-L476RG///////
 InterruptIn inputA1(PA_9);
 InterruptIn inputB1(PA_8);
@@ -25,30 +27,14 @@ DigitalOut IN4(PA_12);
 
 ADXL345 accelerometer(PA_7, PA_6, PA_5, PB_6); // mosi, miso, sck, cs
 BufferedSerial pc(USBTX, USBRX);
+
 // Variables for encoder state
-volatile long position1 = 0;
-volatile long position2 = 0;
-volatile int lastEncoded = 0;
-volatile double speed = 0.0;
-volatile long lastPosition = 0;
-volatile long lastTime = 0;
+volatile uint32_t position1_ticks = 0;
+volatile uint32_t position2_ticks = 0;
 
-void updateMotorPosition1() {
-    if (inputA1.read() != inputB1.read()) {
-        position1++;
-    } else {
-        position1--;
-    }
-}
-
-void updateMotorPosition2() {
-    if (inputA2.read() != inputB2.read()) {
-        position2++;
-    } else {
-        position2--;
-    }
-}
-
+void updateMotorPosition1();
+void updateMotorPosition2();
+void updateSpeed(double &speed1_rpm, double &speed2_rpm);
 void motorA(float duty, int dir);
 void motorB(float duty, int dir);
 
@@ -62,15 +48,11 @@ int main() {
     // Attach the updateEncoder function to be called on the rising edge of inputA
     inputA1.rise(&updateMotorPosition1);
     inputA2.rise(&updateMotorPosition2);
-    //inputA1.fall(&updateMotorPosition);
-    //inputB1.rise(&updateMotorPosition);
-    //inputB1.fall(&updateMotorPosition);
-
-    // Attach the calculateSpeed function to be called every 100ms
     
     // Initialising pwm pins
-    enableA.period(0.001f);
+    enableA.period(0.001f); //Setting a period of 100ms
     enableB.period(0.001f);
+
     // Initialising variables
     int readings[3] = {0, 0, 0};
     
@@ -94,8 +76,50 @@ int main() {
         // 13-bit, sign extended values.
         accelerometer.getOutput(readings);
 
-        printf("X Axis: %i, Y Axis: %i, Z Axis %i, Motor Right: %i, Motor Left %i\n", (int16_t)readings[0], (int16_t)readings[1], (int16_t)readings[2], (int16_t)position1, (int16_t)position2);
+        double speed1_rpm, speed2_rpm;
+        updateSpeed(speed1_rpm, speed2_rpm);
+
+        //std::printf("%i, %i, %i, Motor Right: %ld, Motor Left %ld\n", (int16_t)readings[0], (int16_t)readings[1], (int16_t)readings[2], position1, position2);
+        std::printf("Speed of Motor 1: %f, Speed of Motor 2: %f\n", speed1_rpm, speed2_rpm);
     }
+}
+
+void updateMotorPosition1() {
+    if (inputA1.read() != inputB1.read()) {
+        position1_ticks++;
+    } else {
+        position1_ticks--;
+    }
+}
+
+void updateMotorPosition2() {
+    if (inputA2.read() != inputB2.read()) {
+        position2_ticks++;
+    } else {
+        position2_ticks--;
+    }
+}
+
+void updateSpeed(double &speed1_rpm, double &speed2_rpm) {
+    static int32_t lastPosition1_ticks = 0;
+    static int32_t lastPosition2_ticks = 0;
+    static int32_t lastTime_us = 0;
+
+    static constexpr double us_to_s = 1e-6;
+    const int32_t currentTime_us = us_ticker_read();
+    const double timeDelta_s = static_cast<double>(currentTime_us - lastTime_us) * us_to_s;
+
+    static constexpr int16_t ticksPerCycle = 48;
+    const double positionDelta1_cycles = static_cast<double>(position1_ticks - lastPosition1_ticks) / ticksPerCycle;
+    const double positionDelta2_cycles = static_cast<double>(position2_ticks - lastPosition2_ticks) / ticksPerCycle;
+
+    static constexpr double rps_to_rpm = 1.0 / 60.0;
+    speed1_rpm = (positionDelta1_cycles / timeDelta_s) * rps_to_rpm;
+    speed2_rpm = (positionDelta2_cycles / timeDelta_s) * rps_to_rpm;
+
+    lastPosition1_ticks = position1_ticks;
+    lastPosition2_ticks = position2_ticks;
+    lastTime_us = currentTime_us;
 }
 
 void motorA(float duty, int dir) {
